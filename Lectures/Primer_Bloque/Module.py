0bbd78bcba32f285770728eaa831a7bcc2de91c9
Mandy8808 <arestrada88@gmail.com>
2024-02-28 01:15:36 -0600

    update

--- Lectures/Primer_Bloque/Module.py
+++ Lectures/Primer_Bloque/Module.py
@@ -28,11 +28,56 @@ def Findinterv(f, inter, Ndiv=100):
     encuentra intervalos con raices
     """
     a, b = min(inter), max(inter)
-    interDisc = np.logspace(np.log10(a), np.log10(b), Ndiv) if (abs(a)<1 and a!=0) and (abs(b)<1 and b!=0)\
-                else np.linspace(a, b, Ndiv)              
+    interDisc = np.linspace(a, b, Ndiv)              
     interFinal = []
     for i in range(1, Ndiv):
         inter2 = [interDisc[i-1], interDisc[i]]
         if (SigInt(f, inter2) < 0):
             interFinal.append(inter2)      
     return interFinal
+
+def Bis(f, inter, Nit, error, eps):
+    a, b = min(inter), max(inter)
+    
+    # p0 
+    p0 = (a + b)/2
+    sig = SigInt(f, [a, p0]) 
+    a = p0 if sig>0 else a
+    b = b if sig>0 else p0
+    
+    # pi
+    for i in range(Nit):
+        pi = (a + b)/2
+        epsi = errorDic(error)(a, b) if error=='dist' else errorDic(error)(pi, p0)
+        if abs(epsi)<=eps or f(pi)==0:
+            root = pi
+            break
+        
+        sig = SigInt(f, [a, pi])
+        a = pi if sig>0 else a
+        b = b if sig>0 else pi
+        
+    if i==Nit-1:
+        print('IMPORTANTE: la raiz encontrada no cumple el criterio de eps')
+        root = pi
+    return root
+
+def roo_Bis(f, inter, Nit=1000, error='dist', eps=1e-05, Ndiv=100):
+    """
+    error -> 'dist', 'abs', 'rel', 'relMax'
+    """
+    if not check1(f, inter):
+        sys.exit("Revise los parámetros dados")
+    
+    interF = Findinterv(f, inter, Ndiv=Ndiv)
+    if len(interF)==0:
+        print('No se encontraron roots en el intervalo')
+        roots = None
+    else:
+        roots = []
+        for inter in interF:
+            root = Bis(f, inter, Nit, error, eps)
+            roots.append(root)
+            
+    return roots
+    
