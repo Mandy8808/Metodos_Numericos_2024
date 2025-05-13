a48cac4c61bee657a2fc0df58ddd41a59c4bc96e
Mandy8808 <arestrada88@gmail.com>
2025-05-13 10:37:56 -0600

    update

--- Lectures/Bloque_Cero/matematicas/__init__.py
+++ Lectures/Bloque_Cero/matematicas/__init__.py
@@ -3,7 +3,19 @@
 from .presentacion import hola  # notar que como es local se pone .  Para importar todas las funciones podría usar * 
 from .presentacion import adios
 
+from .multiplicacion_division import multiplicacion
+from .multiplicacion_division import division
+
 # especificamos que modulos se importan con: from <module> import *
-__all__ = ['hola']
+__all__ = [
+    # presentacion
+    'hola', 'adios',
+    # divisionF
+    'division',
+    # multiplicacionF
+    'multiplicacion',
+    #suma y resta
+    'suma', 'resta'
+]
 
 
--- Lectures/Bloque_Cero/matematicas/multiplicacion_division/__init__.py
+++ Lectures/Bloque_Cero/matematicas/multiplicacion_division/__init__.py
@@ -1,1 +1,10 @@
 # matematicas/multiplicacion_division/__init__.py
+from .divisionF import division
+from .multiplicacionF import multiplicacion
+
+__all__ = [
+    # divisionF
+    'division',
+    # multiplicacionF
+    'multiplicacion'
+]
